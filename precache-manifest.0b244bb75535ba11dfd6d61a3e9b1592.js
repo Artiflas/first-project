475013871339b4e70b88f1b14ef27e19c3401341
unknown <Artiflas@gmail.com>
2019-08-05 22:21:02 +0200

    Updates

---
+++ precache-manifest.0b244bb75535ba11dfd6d61a3e9b1592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7244939a4b33ca225ab6d28a76515ef2",
+    "url": "/first-project/index.html"
+  },
+  {
+    "revision": "dd330b23fde0766471e8",
+    "url": "/first-project/static/css/main.b30fd336.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/first-project/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dd330b23fde0766471e8",
+    "url": "/first-project/static/js/main.4bfac674.chunk.js"
+  },
+  {
+    "revision": "34c6a6899d4875285bbd",
+    "url": "/first-project/static/js/runtime~main.72a1c48a.js"
+  }
+]);
